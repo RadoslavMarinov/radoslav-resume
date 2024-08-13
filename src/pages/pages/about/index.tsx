134ad82c9f3da96ac030d97ca5b245b83b288ecd
Radoslav Marinov <radoslavmarinov@Radoslavs-MacBook-Pro.local>
2024-08-13 21:54:52 +0300

    123

--- src/pages/pages/about/index.tsx
+++ src/pages/pages/about/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <div>
         <h1>{count % 5 === 0 ? "Tadaaa!" : ""}</h1>
 
-        <img src="pages/profile-pic.jpeg" alt="" width={100} height={99}></img>
+        <img src="/pages/profile-pic.jpeg" alt="" width={100} height={99}></img>
         <button
           className="bg-slate-700 text-white"
           onClick={() => setCount((prev) => prev + 1)}
