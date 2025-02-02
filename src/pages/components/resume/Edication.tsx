6a6a116e4f5b70e573e0d68db37b0568955ff4ef
Radoslav Marinov <radoslavmarinov@Radoslavs-MacBook-Pro.local>
2025-02-02 17:39:37 +0200

    Add more

--- src/pages/components/resume/Edication.tsx
+++ src/pages/components/resume/Edication.tsx
@@ -7,17 +7,37 @@ const Education: React.FC = () => {
     <div className="w-full">
       <SectionTitle title="Education"></SectionTitle>
       <div className="w-full flex flex-col items-center">
-        <EduSection
-          fromTo="2004 September - 2007 June"
-          description="Second Qualification Level Specialist - Electronics / Technical School
-        “Vasil Dimitrov” - Madan, Bulgaria"
-        />
+        <EduSection>
+          <div>2004 September - 2007 June</div>
+          <div>
+            Second Qualification Level Specialist - Electronics / Technical
+            School “Vasil Dimitrov” - Madan, Bulgaria
+          </div>
+        </EduSection>
 
-        <EduSection
-          fromTo="2007 September - 2013 September"
-          description="Master Degree - Electrical Engineering / University of Transport “Todor
- Kableshkov” - Sofia, Bulgaria"
-        />
+        <EduSection>
+          <div>2007 September - 2013 September</div>
+          <div>
+            Master Degree - Electrical Engineering / University of Transport
+            “Todor Kableshkov” - Sofia, Bulgaria
+          </div>
+        </EduSection>
+        <div className="font-bold self-start text-rsm-secondary">
+          Training courses:
+        </div>
+        <EduSection>
+          <div>2015 June - 2015 September</div>
+          <div>
+            <div className="font-bold">
+              Software Testing / SoftAcad - Sofia, Bulgaria
+            </div>
+            <ul className=" ml-4 list-disc list-inside">
+              <li>Software Development Methodologies (Agile, Waterfall)</li>
+              <li>Quality Assurance</li>
+              <li>Test Tools (Bugzilla, Selenium)</li>
+            </ul>
+          </div>
+        </EduSection>
       </div>
     </div>
   );
--- src/pages/components/resume/EduSection.tsx
+++ src/pages/components/resume/EduSection.tsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { ReactNode } from "react";
+interface TwoChildrenWrapperProps {}
 
 interface EdiSectionProps {
-  // children: React.ReactNode;
-  fromTo: string;
-  description: string;
+  children: [ReactNode, ReactNode];
 }
 
-const EduSection: React.FC<EdiSectionProps> = ({ fromTo, description }) => {
+const EduSection: React.FC<EdiSectionProps> = ({ children }) => {
+  if (!Array.isArray(children) || children.length !== 2) {
+    throw new Error("TwoChildrenWrapper requires exactly two children.");
+  }
+
+  const [from, to] = children;
+
   return (
-    <div className="w-full py-6 flex flex-row gap-6 flex-wrap lg:flex-nowrap lg:justify-start justify-center">
-      <div className="text-rsm-tertiary lg:w-1/4">{fromTo}</div>
-      <div className="lg:w-3/4">{description}</div>
+    <div
+      className={`
+        w-full py-6 flex flex-row gap-6 flex-wrap lg:flex-nowrap lg:justify-start justify-center
+      `}
+    >
+      <div className="text-rsm-tertiary lg:w-1/4">{from}</div>
+      <div className="lg:w-3/4">{to}</div>
     </div>
   );
 };
